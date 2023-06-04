ac4e438580a81e95c384a0f919400713882e0717
Sudhanshu09102000 <127600086+Sudhanshu09102000@users.noreply.github.com>
2023-06-04 16:41:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
- tablinks= document.getElementsByClassName("tab-links"); 
- tabcontents= document.getElementsByClassName("tab-contents");
+tablinks= document.getElementsByClassName("tab-links"); 
+tabcontents= document.getElementsByClassName("tab-contents");
 function opentab(tabname){
-    for(tablink of tablinks ){
-        tablink.classList.remove("active-link")
-    }
-    for(tabcontent of tabcontents ){
-        tabcontent.classList.remove("active-tab")
-    }
-    event.currentTarget.classList.add("active-link")
-    document.getElementById(tabname).classList.add("active-tab")
+   for(tablink of tablinks ){
+       tablink.classList.remove("active-link")
+   }
+   for(tabcontent of tabcontents ){
+       tabcontent.classList.remove("active-tab")
+   }
+   event.currentTarget.classList.add("active-link")
+   document.getElementById(tabname).classList.add("active-tab")
 }
 
 
@@ -17,10 +17,10 @@ function opentab(tabname){
 var sidemenu= document.getElementById("sidemenu");
 
 function openmenu(){
-    sidemenu.style.right="0";
+   sidemenu.style.right="0";
 }
 function closemenu(){
-    sidemenu.style.right="-200px";
+   sidemenu.style.right="-200px";
 }
 
 
@@ -28,10 +28,12 @@ function closemenu(){
 // improvements
 
 document.getElementById("my-work").addEventListener("mouseover", ()=>{
-    document.getElementById("my-work").innerText="My Work (in-progress)"
+   document.getElementById("my-work").innerText="My Work (in-progress)"
 })
 document.getElementById("my-work").addEventListener("mouseleave", ()=>{
-    document.getElementById("my-work").innerText="My Work"
+   document.getElementById("my-work").innerText="My Work"
 })
 
 var a= window.innerWidth;
+
+
